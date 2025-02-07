1b19a5956b13f2ad9dffc0e9128bdebcf7961d86
尤雨东 <root@ltpp.vip>
2025-02-07 22:53:41 +0800

    feat:

--- sh/init.sh
+++ sh/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git remote add jihulab git@jihulab.com:ltpp-universe/leetcodeandacwingrank.git;
+git remote add gitee git@gitee.com:ltpp-universe/leetcodeandacwingrank.git;
 git remote add github git@github.com:ltpp-universe/LeetcodeAndAcwingRank.git;
 git remote add origin git@github.com:ltpp-universe/LeetcodeAndAcwingRank.git;
